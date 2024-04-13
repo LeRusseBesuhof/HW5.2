22a14a340c92172d6b5eedf2bd0054d8827c514e
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-04-13 18:17:53 +0700

    Initial Commit

---
+++ HW5.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5.2
+//
+//  Created by Павел Градов on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
